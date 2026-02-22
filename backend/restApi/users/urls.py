bb6b98d6ea137d1cc73f0e8c4ee23fbb7a5089ec
luciatamarit <202311457@alu.comillas.edu>
2026-02-22 21:02:17 +0100

    error lucia

--- backend/restApi/users/urls.py
+++ backend/restApi/users/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from .views import UsageView
 
+# urlpatterns = [
+#     path("usage/", UsageView.as_view(), name="usage"),
+# ]
+
+
+from django.urls import path
+from .views import UsageView, RegisterView, ProfileView, LogoutView, ChangePasswordView
+
 urlpatterns = [
     path("usage/", UsageView.as_view(), name="usage"),
+
+    path("users/register/", RegisterView.as_view(), name="register"),
+    path("users/profile", ProfileView.as_view(), name="profile"),
+    path("users/profile/password", ChangePasswordView.as_view(), name="change_password"),
+    path("users/logout", LogoutView.as_view(), name="logout"),
 ]
